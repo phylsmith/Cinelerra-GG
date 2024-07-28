7e4aa7bd5d25f46c14b04536095371ac22989e2f
Good Guy <good1.2guy@gmail.com>
2024-07-28 12:14:57 -0600

    trivial changes in conjunction with x265 multibit, update About monthly txt, fix bld.sh comments as a reminder

--- cinelerra-5.1/bld.sh
+++ cinelerra-5.1/bld.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #  IMPORTANT comments below to change the configure line
-# For python version 12, add --without-lv2
 # For newer operating system versions, add --enable-libsvtav1
 # For really old versions, such as ubuntu 16, add --enable-libaom=no
+# To add OpenCV plugins, add --with-opencv=sta,tar=http://cinelerra-gg.org/download/opencv/opencv-20200306.tgz
 ( ./autogen.sh
   ./configure --with-single-user --with-booby
   make && make install ) 2>&1 | tee log
