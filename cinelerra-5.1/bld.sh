c5feec62b065d5ac62445c5eb91263be8a104d7f
Phyllis Smith <phylsmith2017@gmail.com>
2025-12-30 14:03:27 -0700

    Minor changes in comments for msg/txt and bld.txt

--- cinelerra-5.1/bld.sh
+++ cinelerra-5.1/bld.sh
@@ -2,7 +2,8 @@
 #  IMPORTANT comments below to change the configure line
 # For newer operating system versions, add --enable-libsvtav1
 # For really old versions, such as ubuntu 16, add --enable-libaom=no
-# To add OpenCV plugins, add --with-opencv=sta,tar=http://cinelerra-gg.org/download/opencv/opencv-20200306.tgz
+# Faster compile if include --disable-x265_hidepth
+# To add OpenCV plugins, add --with-opencv=sta,tar=https://download.cinelerra-gg.org/download.php?file=opencv%2Fopencv-20200306.tgz
 ( ./autogen.sh
   ./configure --with-single-user --with-booby
   make && make install ) 2>&1 | tee log
