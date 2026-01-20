1869ec33045187605171a15a2c8423f70e14783c
Phyllis Smith <phylsmith2017@gmail.com>
2026-01-20 11:23:52 -0700

    Credit Andrew - minor mods needed for building without thirdparty + year changed

--- cinelerra-5.1/cinelerra/versioninfo.h
+++ cinelerra-5.1/cinelerra/versioninfo.h
@@ -9,7 +9,7 @@
 #define COPYRIGHTTEXT1 "(c) 2006-2019 Heroine Virtual Ltd. by Adam Williams\n"
 #define COPYRIGHTTEXT2 "2007-2020 mods for Cinelerra-GG by W.P.Morrow aka goodguy\n"
 #define COPYRIGHTTEXT3 "2003-2017 mods for Cinelerra-CV by CinelerraCV team\n"
-#define COPYRIGHTTEXT4 "2015-2025 mods for Cinelerra-GG by Cinelerra-GG team\n"
+#define COPYRIGHTTEXT4 "2015-2026 mods for Cinelerra-GG by Cinelerra-GG team\n"
 
 #undef COMPILEDATE
 
