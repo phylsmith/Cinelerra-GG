a2709c364807abaa8b9553491ee4e9eaf15c735d
Phyllis Smith <phylsmith2017@gmail.com>
2026-03-27 14:13:25 -0600

    Credit Andrew - patch x265 to allow multiple CPUs usage for faster compile + minor updates to some text files

--- cinelerra-5.1/bld.sh
+++ cinelerra-5.1/bld.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #  IMPORTANT comments below to change the configure line
 # For newer operating system versions, add --enable-libsvtav1
-# For really old versions, such as ubuntu 16, add --enable-libaom=no
-# Faster compile if include --disable-x265_hidepth
+# For older versions, you may need to update nasm and/or cmake or you
+# can disable any thirdparty library that has cmake or assembly errors.
+# For example an older ubuntu 16 version might need:  add --enable-libaom=no
 # To add OpenCV plugins, add --with-opencv=sta,tar=https://download.cinelerra-gg.org/download.php?file=opencv%2Fopencv-20200306.tgz
 ( ./autogen.sh
   ./configure --with-single-user --with-booby
